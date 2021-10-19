552d9470e1a32d618b285d084fc64925a53ee4e3
Alzter Aquino <alztr2908@gmail.com>
2021-10-19 20:22:57 +0800

    init script files for carousel and tab

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const subjectGeneral = document.getElementsByClassName("subject-button-general")
+const subjectGeneral = document.getElementsByClassName(
+    "subject-button-general"
+)[0]
 const subjectSpecialized = document.getElementsByClassName(
     "subject-button-specialize"
-)
+)[0]
+const subjectContent = document.getElementsByClassName("subject")[0]
+
+// WIP
+// Toggle buttons for general and specialized courses
+
+subjectGeneral.addEventListener("click", () => {
+    subjectContent.innerHTML = subjectGeneral.textContent
+    console.log(subjectGeneral.textContent)
+})
+subjectSpecialized.addEventListener("click", () => {
+    subjectContent.innerHTML = subjectSpecialized.textContent
+})
